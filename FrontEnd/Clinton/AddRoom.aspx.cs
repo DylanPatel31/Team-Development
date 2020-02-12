c81bef3c8b33bd625e377313431eb40abae6f2d2
p17190399 <p17190399@GWHXX282WIN019W>
2020-02-12 14:28:46 +0000

    Done the codes for admin

---
+++ FrontEnd/Clinton/AddRoom.aspx.cs
@@ -0,0 +1,157 @@
+﻿using Hotel_Delux.Model_classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hotel_Delux.admin.room
+{
+    public partial class AddRoom : System.Web.UI.Page
+    {
+        string message = "";
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["username"] == null || Session["roleType"] == null || checkRoletype(Session["roleType"].ToString(), "admin"))
+            {
+                Response.Redirect("/admin/Index.aspx", false);
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["username"] == null || Session["roleType"] == null || checkRoletype(Session["roleType"].ToString(), "admin"))
+                {
+                    Response.Redirect("/admin/Index.aspx", false);
+                }
+               
+
+            }
+        }
+        public bool checkRoletype(string session, string compareString)
+        {
+            if (string.Compare(session, compareString) != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int value = addRoom(roomNo.Value, roomPrice.Value, roomType.Value, Convert.ToInt32(roomAvailable.Value), uploadFile1, uploadFile2);
+            if (value >= 1)
+            {
+                message = "Successfully Room added";
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('" + message + "',true)", true);
+                Console.WriteLine(message);
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('" + message + "',false)", true);
+                Console.WriteLine(message);
+            }
+        }
+        public int addRoom(string roomNo, string roomPrice, string roomType, int roomAvailable, FileUpload fileUpload, FileUpload fileUpload1)
+        {
+            string imgName, imgName1,imgPath,imgPath1;
+            imgName = fileUpload.FileName.ToString();
+            imgName1 = fileUpload1.FileName.ToString();
+            string relativePathToUpload = "../../asset/images/rooms/";
+            string relativePath = "/asset/images/rooms/";
+            string absolute = Server.MapPath(relativePath);
+            string exactPath = Path.GetFullPath(relativePath);
+            imgPath = relativePath + imgName;
+            imgPath1 = relativePath + imgName1;
+            string imgPathForUpload = relativePathToUpload + imgName;
+            string imgPathForUpload1 = relativePathToUpload + imgName1;
+            if (fileUpload.PostedFile.ContentLength > 5048576) // 5120 KB means 5MB
+            {
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('File is too big',false)", true);
+                fileUpload.Focus();
+            }
+            else
+            {
+                if (!File.Exists(Server.MapPath(imgPathForUpload)))
+                {
+                    fileUpload.SaveAs(Server.MapPath(relativePathToUpload + imgName));
+                }
+            }
+            if (fileUpload1.PostedFile.ContentLength > 5048576) // 5120 KB means 5MB
+            {
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('File is too big',false)", true);
+                fileUpload1.Focus();
+            }
+            else{
+                if (!File.Exists(Server.MapPath(imgPathForUpload1)))
+                {
+                    fileUpload1.SaveAs(Server.MapPath(relativePathToUpload + imgName1));
+                }      
+            }
+            
+            int effectedRows = 0;
+            int roomID = 0;
+            SqlConnection conn = new SqlConnection(DbString.connectionString);
+            SqlCommand cmd;
+            cmd = new SqlCommand("RoomSearch", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@roomNo", roomNo));
+            try
+            {
+                conn.Open();
+                int numberOfRows = Convert.ToInt32(cmd.ExecuteScalar());
+                if (numberOfRows >= 1)
+                {
+                    message = "Room Number already exists.";
+                    effectedRows = 0;
+                }
+                else
+                {
+                    cmd = new SqlCommand("RoomAdd", conn);
+                    //Instruct the command object to execute it
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@roomNo", roomNo));
+                    cmd.Parameters.Add(new SqlParameter("@roomPrice", roomPrice));
+                    cmd.Parameters.Add(new SqlParameter("@roomType", roomType));
+                    cmd.Parameters.Add(new SqlParameter("@roomAvailable", roomAvailable));
+                    roomID = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    cmd = new SqlCommand("RoomImagesAdd", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@roomID", roomID));
+                    cmd.Parameters.Add(new SqlParameter("@imgName", imgName));
+                    cmd.Parameters.Add(new SqlParameter("@imgPath", imgPath));
+                    cmd.Parameters.Add(new SqlParameter("@imgName1", imgName1));
+                    cmd.Parameters.Add(new SqlParameter("@imgPath1", imgPath1));
+                    effectedRows= Convert.ToInt32(cmd.ExecuteNonQuery());
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                Console.WriteLine(ex.Message);
+            }
+
+            return effectedRows;
+        }
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            roomNo.Value = "";
+            roomPrice.Value = "";
+            roomType.Value = "";
+        }
+        protected void logout_ServerClick(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("/admin/Index.aspx");
+        }   
+    }
+}
---
+++ FrontEnd/Clinton/Index.aspx.cs
@@ -0,0 +1,131 @@
+﻿using Hotel_Delux.Model_classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hotel_Delux.admin.room
+{
+    public partial class index : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(DbString.connectionString);
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["username"] == null || Session["roleType"] == null || checkRoletype(Session["roleType"].ToString(), "admin"))
+            {
+                Response.Redirect("/admin/Index.aspx", false);
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["username"] == null || Session["roleType"] == null || checkRoletype(Session["roleType"].ToString(), "admin"))
+                {
+                    Response.Redirect("/admin/Index.aspx", false);
+                }
+                gvbind();
+            }
+        }
+        protected void gvbind()
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("RoomList", conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                conn.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                }
+                else
+                {
+                    ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                    int columncount = GridView1.Rows[0].Cells.Count;
+                    GridView1.Rows[0].Cells.Clear();
+                    GridView1.Rows[0].Cells.Add(new TableCell());
+                    GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+                    GridView1.Rows[0].Cells[0].Text = "No Records Found";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int effectedRows = 0;
+            string message = "";
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            string roomNo = GridView1.DataKeys[e.RowIndex].Values["RoomNo"].ToString();
+            SqlCommand cmd = new SqlCommand("RoomDelete", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.Parameters.Add(new SqlParameter("@roomNo", roomNo));
+                conn.Open();
+                effectedRows = cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                Console.WriteLine(ex.Message);
+            }
+            if (effectedRows > 0)
+            {
+                message = "Successfully Room Deleted.";
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('" + message + "',true)", true);
+                gvbind();
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "popUp('" + message + "',false)", true);
+            }
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            string roomNo = GridView1.DataKeys[e.NewEditIndex].Values["RoomNo"].ToString();
+            Session.Add("roomNo", roomNo);
+            Response.Redirect("EditRoom.aspx");
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+        protected void GridView1_RowSelecting(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView1.SelectedRow;
+            string roomNO = row.Cells[0].Text;
+            Session.Add("roomNO", roomNO);
+            Response.Redirect("ViewRoom.aspx");
+        }
+        public bool checkRoletype(string session, string compareString)
+        {
+            if (string.Compare(session, compareString) != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        protected void logout_ServerClick(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("/admin/Index.aspx");
+        }
+    }
+}
